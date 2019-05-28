d6650ca6f3d414fb9949b1550c2161de39539872
gurpreetcigs <gurpreet.cigs@gmail.com>
2019-05-28 18:38:44 +0530

    Implementing Material design to angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,22 @@
-import {BrowserModule} from '@angular/platform-browser';
+import {  BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import {AppComponent} from './app.component';
-import {ScheduleItemComponent} from './schedule-item/schedule-item.component';
-
-import {MDBBootstrapModule} from 'angular-bootstrap-md';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import {AngularFireModule} from '@angular/fire';
-import {AngularFirestoreModule} from '@angular/fire/firestore';
-import {FirebaseService} from './services/firebase.service';
+import {AppComponent} from './app.component'
+import {HelloComponent} from './hello.component';
+// import { environment } from '../environments/environment';
+import { CustomModule } from './custom.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ScheduleItemComponent
+    HelloComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MDBBootstrapModule.forRoot(),
-    FormsModule,
-    ReactiveFormsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule
+    CustomModule
   ],
-  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
